22bf4cb7a81c3e4f0143f4a2b4d795ed3e9b229d
draktate <draktate@gmail.com>
2023-02-24 13:47:58 +0530

    change1

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -14,9 +14,9 @@ const Home = () => {
       <Header/>
       <div className="homeContainer">
         <Featured/>
-        <h1 className="homeTitle">Browse by property type</h1>
+        <h1 className="homeTitle">Properties by property type</h1>
         <PropertyList/>
-        <h1 className="homeTitle">Homes guests love</h1>
+        <h1 className="homeTitle">Local people like:</h1>
         <FeaturedProperties/>
         <MailList/>
         <Footer/>
